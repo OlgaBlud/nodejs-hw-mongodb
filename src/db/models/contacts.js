8a119bcaafff0101366c3bbae0ad3aa943dd2b29
Olga Blud <olga.blud@gmail.com>
2024-10-04 16:07:18 +0300

    get all contacts

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -30,4 +30,4 @@ const contactsSchema = new Schema(
     versionKey: false,
   },
 );
-export const contactsCollection = model('contacts', contactsSchema);
+export const ContactsCollection = model('contacts', contactsSchema);
