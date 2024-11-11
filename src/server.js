3d3e13ce77497489b3e2d44838fe1cc61c29fb39
Olga Blud <olga.blud@gmail.com>
2024-11-11 17:40:06 +0200

    swagger done

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import cookieParser from 'cookie-parser';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { UPLOAD_DIR } from './constants/constants.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
@@ -37,6 +38,8 @@ export const setupServer = () => {
 
   app.use(router);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
